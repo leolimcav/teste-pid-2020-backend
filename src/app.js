990c9ad4110ffadc38c498973c34625e1cc46620
Leonardo <leonardo123k@gmail.com>
2020-02-24 23:25:46 -0300

    feat: update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
-    this.server.use(express.urlencoded());
     this.server.use(cors());
     this.server.use(
       '/files',
